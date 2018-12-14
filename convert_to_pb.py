c59b4c88adf747559cea4622cf6ca46bdecc34d7
xjh19971 <414581775@qq.com>
2018-12-13 22:25:00 -0800

    convert

---
+++ convert_to_pb.py
@@ -0,0 +1,77 @@
+import argparse
+
+import tensorflow as tf
+from keras import backend as K
+import tensorflow.contrib.tensorrt as trt
+from config_reader import config_reader
+from model.cmu_model_mobilenet import get_testing_model
+
+
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True,input_names=None):
+    """
+    Freezes the state of a session into a pruned computation graph.
+
+    Creates a new computation graph where variable nodes are replaced by
+    constants taking their current value in the session. The new graph will be
+    pruned so subgraphs that are not necessary to compute the requested
+    outputs are removed.
+    @param session The TensorFlow session to be frozen.
+    @param keep_var_names A list of variable names that should not be frozen,
+                          or None to freeze all the variables in the graph.
+    @param output_names Names of the relevant graph outputs.
+    @param clear_devices Remove the device directives from the graph for better portability.
+    @return The frozen graph definition.
+    """
+    from tensorflow.python.framework.graph_util import convert_variables_to_constants
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        # Graph -> GraphDef ProtoBuf
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ""
+        frozen_graph = convert_variables_to_constants(session, input_graph_def,
+                                                      output_names, freeze_var_names)
+        return frozen_graph,output_names,input_names
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='model/keras/model.h5', help='path to the weights file')
+
+    args = parser.parse_args()
+    keras_weights_file = args.model
+
+
+    print('start processing...')
+
+    # load model
+
+    # authors of original model don't use
+    # vgg normalization (subtracting mean) on input images
+    #model = get_testing_model()
+    #model.load_weights(keras_weights_file)
+
+    # load config
+    params, model_params = config_reader()
+    K.set_learning_phase(0)
+    # load model
+
+    # authors of original model don't use
+    # vgg normalization (subtracting mean) on input images
+    model = get_testing_model()
+    model.load_weights(keras_weights_file)
+    frozen_graph, output_names, input_names = freeze_session(K.get_session(),
+                                                             output_names=[out.op.name for out in model.outputs],
+                                                             input_names=[out.op.name for out in model.inputs])
+    tf.train.write_graph(frozen_graph, ".", "tf_model.pb", as_text=False)
+    '''    trt_graph = trt.create_inference_graph(
+        input_graph_def=frozen_graph,
+        outputs=output_names,
+        max_batch_size=1,
+        max_workspace_size_bytes=1 << 25,
+        precision_mode='FP16',
+        minimum_segment_size=50
+    )'''
