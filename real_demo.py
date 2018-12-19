b8ac7664fa71c693706af5403a84cbff07be26fd
xjh19971 <414581775@qq.com>
2018-12-19 05:01:02 -0800

    video change

--- real_demo.py
+++ real_demo.py
@@ -293,7 +293,7 @@ if __name__ == '__main__':
     # Video writer
     output_fps = input_fps / frame_rate_ratio
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(video_output,fourcc, output_fps, (640, 512))
+    out = cv2.VideoWriter(video_output,fourcc, output_fps, (input_image.shape[1]/2, input_image.shape[0]/2))
 
     tf_config = tf.ConfigProto()
     tf_config.gpu_options.allow_growth = True
@@ -346,8 +346,8 @@ if __name__ == '__main__':
     i = 0 # default is 0
     while(cam.isOpened()) and ret_val == True and i < ending_frame:
         if i%frame_rate_ratio == 0:
-            input_image = cv2.resize(input_image, (640, 512),
-                                 interpolation=cv2.INTER_CUBIC)
+            scale=0.5
+            imageToTest = cv2.resize(input_image, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
             tic = time.time()
 
             # generate image with body parts
