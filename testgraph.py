005384c7e05da5098a4c6dca8bf45699290bd5c7
xjh19971 <414581775@qq.com>
2018-12-21 01:30:59 -0800

    try inception

--- testgraph.py
+++ testgraph.py
@@ -4,5 +4,7 @@ from tensorflow.python.platform import gfile
 # 这是从二进制格式的pb文件加载模型
 graph = tf.get_default_graph()
 graphdef = graph.as_graph_def()
-graphdef.ParseFromString(gfile.FastGFile("/data/TensorFlowAndroidMNIST/app/src/main/expert-graph.pb", "rb").read())
+graphdef.ParseFromString(gfile.FastGFile("./tf_model.pb", "rb").read())
 _ = tf.import_graph_def(graphdef, name="")
+summary_write = tf.summary.FileWriter("./logdir" , graph)
+
