a15c315ec862e2d15494cbcace6aada35928eeba
xjh19971 <414581775@qq.com>
2018-12-28 04:18:05 -0800

    inception

--- model/cmu_model_inception_improve.py
+++ model/cmu_model_inception_improve.py
@@ -206,7 +206,7 @@ def stage1_block(x, num_p, branch, weight_decay):
 def stageT_block(x, num_p, stage, branch, weight_decay):
     bn_axis = 1 if K.image_data_format() == 'channels_first' else -1
     # Block 1
-    x = conv(x, 64, 1, "Mconv2_stage%d_L%d" % (stage, branch), (weight_decay, 0))
+    x = conv(x, 64, 1, "Mconv1_stage%d_L%d" % (stage, branch), (weight_decay, 0))
     x = BatchNormalization(axis=bn_axis, epsilon=1e-5, momentum=0.9)(x)
     x1 = relu(x)
     x = conv(x, 64, 3, "Mconv2_stage%d_L%d" % (stage, branch), (weight_decay, 0))
