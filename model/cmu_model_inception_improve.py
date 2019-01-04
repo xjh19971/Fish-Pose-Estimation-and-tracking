b4df20b6d87d918b09ea9508aa4565733968f15a
xjh19971 <414581775@qq.com>
2019-01-04 03:48:39 -0800

    mobile remove

--- model/cmu_model_inception_improve.py
+++ model/cmu_model_inception_improve.py
@@ -100,23 +100,11 @@ def relu(x):
 
 
 def conv(x, nf, ks, name, weight_decay, strides=(1,1)):
-    bn_axis = 1 if K.image_data_format() == 'channels_first' else -1
-    if ks!=1:
-        x = DepthwiseConv2D((ks, ks),
-                            padding='same',
-                            depth_multiplier=1,
-                            use_bias=False,
-                            strides=strides,
-                            kernel_regularizer=l2(weight_decay[0]),
-                            )(x)
-        x = BatchNormalization(axis=bn_axis, epsilon=1e-5, momentum=0.9)(x)
-        x = relu(x)
-        x = Conv2D(nf, (1,1), padding='same', strides=1, use_bias=False, name=name,
-               kernel_regularizer=l2(weight_decay[0]))(x)
-    else:
-        x = Conv2D(nf, (1, 1), padding='same', strides=strides, use_bias=False, name=name,
-                   kernel_regularizer=l2(weight_decay[0]))(x)
-
+    kernel_reg = l2(weight_decay[0]) if weight_decay else None
+    x = Conv2D(nf, (ks, ks), padding='same', name=name, strides=strides,
+                   kernel_regularizer=kernel_reg,
+                   kernel_initializer=random_normal(stddev=0.01),
+                   use_bias=False)(x)
     return x
 
 
